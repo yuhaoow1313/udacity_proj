fdab478ef15d6d36203ceb5c63588c3021e906e8
yuhaoow1313 <95628406+yuhaoow1313@users.noreply.github.com>
2022-05-31 22:54:15 +0800

    Add files via upload

--- Project_2/utils.py
+++ Project_2/utils.py
@@ -12,13 +12,13 @@ def load_model(model_path):
     final_model_path = './' + model_path
     reloaded_keras_model = tf.keras.models.load_model(final_model_path,custom_objects={'KerasLayer':hub.KerasLayer},compile=False)
     return reloaded_keras_model
-    
+
 def process_image(image):
     image = tf.cast(image, tf.float32)
     image = tf.image.resize(image, (224,224))
     image /= 255
     image = image.numpy().squeeze()
-    
+
     return image
 
 def predict_function(image_path, model, top_k=5):
@@ -28,16 +28,16 @@ def predict_function(image_path, model, top_k=5):
 
     processed_test_image = process_image(test_image)
     processed_test_image_batch = np.expand_dims(processed_test_image, axis=0)
-    
+
     pred = model.predict(processed_test_image_batch)
     prob = -np.sort(-pred[0])[:top_k]
     prob = prob.tolist()
-    
+
     classes = []
     for i in prob:
         idx = np.where(pred[0] == i)[0][0]
-        classes.append(str(idx))
-        
-    classes=np.asarray(classes) 
+        classes.append(str(idx+1))
+
+    classes=np.asarray(classes)
 
     return prob, classes
